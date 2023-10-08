fe3dbe48bbdf923b70bcfea81122d374871bedcf
mabar84 <mabar@tut.by>
2023-10-09 00:28:19 +0300

    settings-button added

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,42 +1,65 @@
-import React from "react";
-import styled from "styled-components";
+import React, {useState} from "react";
+import styled, {css} from "styled-components";
 import {myTheme} from "../../styles/Theme.styled";
 import {Logo} from "../logo/Logo";
 
 export const Header = () => {
+
+    const [collapsed, setCollapsed] = useState<boolean>(false)
+    const onclickHandler = () => {
+        console.log(123)
+        console.log(collapsed)
+        setCollapsed(!collapsed)
+    }
+
+
     return (
-        <StyledHeader>
+        <StyledHeader $collapsed={collapsed}>
+
+
+            <div className="container">
+                <p>settings</p>
+                <p>settings</p>
+                <p>settings</p>
+                <p>settings</p>
+            </div>
+
+
             <div className="container">
                 <Logo/>
-
+                <button onClick={onclickHandler}>Настройки</button>
             </div>
+
 
         </StyledHeader>
     );
 };
 
-const StyledHeader = styled.header`
-  height: 50px;
-  position: sticky;
-  top: 0;
+
+const StyledHeader = styled.header<{ $collapsed: boolean }>`
+  height: 100vh;
   z-index: 1;
-  display: flex;
-  justify-content: end;
-  gap: 50px;
   border-radius: 0 0 10px 10px;
-  background-color: ${myTheme.colors.primary_background};
   color: ${myTheme.colors.text};
+  background-color: ${myTheme.colors.primary_background};
+  transform: translateY(calc(50px - 100vh));
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  transition: transform 2s;
+
+  ${(props) => props.$collapsed && css`
+    transform: translateY(0);
 
 
-  //height: 100vh;
-  
-
+  `}
   & .container {
     padding: 5px;
     width: 100%;
     display: flex;
     align-items: center;
-    justify-content: start;
+    gap: 20px;
+    justify-content: space-between;
 
     ${myTheme.media.less1080} {
       gap: 15px;
